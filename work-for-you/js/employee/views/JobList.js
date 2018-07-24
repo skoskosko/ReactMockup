310d2f9e77d06cc0d061b8a9a57876214c3cf7c1
Esko Takku <esko.takku@gmail.com>
2018-07-24 22:15:27 +0300

    asd

--- work-for-you/js/employee/views/JobList.js
+++ work-for-you/js/employee/views/JobList.js
@@ -56,14 +56,14 @@ class JobList extends Component {
 }
 export default JobList
 
-// const styles = StyleSheet.create ({
-//    container: {
-//       padding: 10,
-//       marginTop: 3,
-//       backgroundColor: '#d9f9b1',
-//       alignItems: 'center',
-//    },
-//    text: {
-//       color: '#4f603c'
-//    }
-// })
+const styles = StyleSheet.create ({
+   container: {
+      padding: 10,
+      marginTop: 3,
+      backgroundColor: '#d9f9b1',
+      alignItems: 'center',
+   },
+   text: {
+      color: '#4f603c'
+   }
+})
